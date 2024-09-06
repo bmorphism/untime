71ec7b15e38f38d0bc081971e45827ab4420072f
barton <barton@vibes.lol>
2024-09-06 22:53:44 +0000

    Initial Commit

---
+++ time Watch App/timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeApp.swift
+//  time Watch App
+//
+//  Created by barton on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct time_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
